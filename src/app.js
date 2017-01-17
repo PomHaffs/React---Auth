a613baedc20345d3f35c50d140543007bb5b634d
PomHaffs <tomashaffenden@icloud.com>
2017-01-17 12:55:38 +1100

    Added basic loginform component

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'React';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBbPsLPfZD0U_odcmFbsXcZ0zUK-OaJOkw',
+      authDomain: 'authentication-6d01f.firebaseapp.com',
+      databaseURL: 'https://authentication-6d01f.firebaseio.com',
+      storageBucket: 'authentication-6d01f.appspot.com',
+      messagingSenderId: '561377822968'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App I Did!</Text>
+        <LoginForm />
       </View>
     );
   }
-}
+};
 
 export default App;
